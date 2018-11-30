850d9e10cba48cb1c2fadcdaa5e1912e97e99b79
Watson Ndethi <wndethi@gmail.com>
2018-11-30 11:41:37 +0300

    Added geocodes of facilities to google map, geolocation of current user

--- public/js/gmaps.js
+++ public/js/gmaps.js
@@ -9,7 +9,7 @@ function initMap() {
     
     var map = new google.maps.Map(document.getElementById('locator'), {
       center: myLatLng,
-      zoom: 6
+      zoom: 12
     });
 
     //Infowindow
@@ -17,10 +17,17 @@ function initMap() {
     //County
     var geocoder = new google.maps.Geocoder;
     var infoWindow = new google.maps.InfoWindow;
-    var county;
+    var marker = new google.maps.Marker;
+  
+
+    //Special Icon to mark current location of user
+    var icon ={
+      url:"./icons/map-marker-with-a-person-shape.png",
+      scaledSize: new google.maps.Size(50,50)
+    }
 
     //Set Facility Markers across Map
-    setFacilityMarkers();
+    setFacilityMarkers(map);
 
     //Use Geolocation service to get center
     if (navigator.geolocation) {
@@ -30,6 +37,7 @@ function initMap() {
             lng: position.coords.longitude
           };
 
+          console.log(pos)
           //Call Reverse Geocoding service
           geocodeLatLng(geocoder, map, infoWindow);
 
@@ -39,8 +47,17 @@ function initMap() {
                 console.log(results)
                   if(status === 'OK'){
                       if(results[0]){
-                          infoWindow.setContent(results[0].formatted_address);
-                          infoWindow.open(map);
+                          marker.setIcon(icon);
+                          marker.setPosition(pos);
+                          marker.setMap(map);
+                          infoWindow.setContent('You are at <b>'+results[0].formatted_address+'</b>');
+                          infoWindow.setPosition(pos);
+                          marker.addListener('click', function(){
+                            infoWindow.setContent('You are at <b>'+results[0].formatted_address+'</b>');
+                            infoWindow.setPosition(pos);
+                            infoWindow.open(map,marker);
+                          });
+                          
                       }
                       else{
                           window.alert('No results found.');
@@ -52,11 +69,9 @@ function initMap() {
               })
           }
 
-          infoWindow.setPosition(pos);
-          //infoWindow.setContent('Your Location');
-          infoWindow.open(map);
+          infoWindow.open(map,marker);
           map.setCenter(pos);
-          map.setZoom(10);
+          map.setZoom(13);
         }, function() {
           handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -66,11 +81,35 @@ function initMap() {
       }
    
     //Get facility location and details, loop through them and plant markers across map
-    function setFacilityMarkers(){
+    function setFacilityMarkers(map){
       axios.get('api/facilities').then(function(response){
           $.each(response.data, function(key, value){
-            console.log(value)
-            //var LatLng = new google.maps.LatLng(re)
+
+            //Get address in lang, lat [parseFloat to convert value from string to float]
+            var LatLng = {lat: parseFloat(value.Latitude), lng: parseFloat(value.Longitude)}
+
+
+
+            //Get title of marker
+            var markerTitle = value.facility;
+
+            //Set facility info
+            var facilityInfo='<div><p><b>'+value.facility+'</b></p><p>'+value.Level+', '+value.County+', '+value.Sub_County+'</p></div>';
+
+            //Set marker variable
+            var marker = new google.maps.Marker({
+                position:LatLng,
+                title:markerTitle
+            });
+
+            //Add marker to map
+            marker.setMap(map);
+            marker.addListener('click', function(){
+              infoWindow.setContent(facilityInfo);
+              infoWindow.setPosition(LatLng);
+              infoWindow.open(map, marker);
+            })
+
           })
       }).catch(function(error){
 
@@ -92,7 +131,7 @@ function initMap() {
 
     //Style Geojson DataLayer
     map.data.setStyle({
-        fillColor: '#FA8072',
+        fillColor: '',
         strokeWeight: 0.5,
         strokeColor: 'white'
       });
